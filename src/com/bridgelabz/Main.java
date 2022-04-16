82f3aafe865283777e7504f005dcbf29c2382b57
furiousrage <kumarvicky25jsr@gmail.com>
2022-04-16 21:03:37 +0530

    some comments

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         // roshini
+        /*
+        master -> which is running or prod server
+        Pre-prod -> 0-5% 0-1% -> release
+        QA-server -> 100%
+        dev ->
+        pay-tm-payment ->  "abc" -> "abcd"
+         */
     }
 
 
